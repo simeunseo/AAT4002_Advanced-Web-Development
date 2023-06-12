81dfacd60287e660c507b442e0e3430bdea84568
simeunseo <rkdcjfehowl@naver.com>
2023-06-12 23:52:25 +0900

    feat: mongoDB 연결

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect("mongodb+srv://admin:Y5G4G5niY2APDPMK@cluster0.5nyf8tl.mongodb.net/")
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
