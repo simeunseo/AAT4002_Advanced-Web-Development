764860d1eff0bf7adf5a957acdda6b9e13361e9f
simeunseo <rkdcjfehowl@naver.com>
2023-06-28 00:40:06 +0900

    style: theme에 그라디언트 추가

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -2,6 +2,7 @@ import { DefaultTheme, css } from "styled-components";
 
 const colors = {
   primary: "#98E044",
+  liner_primary: "linear-gradient(180deg, #98E044 0%, #EBF9DA 19.79%, #FFF 52.60%)",
 };
 
 const fonts = {
