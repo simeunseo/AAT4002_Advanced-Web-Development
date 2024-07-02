5c8969996d3964cc384865333436ba6692600f29
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-07-02 23:17:02 +0900

    feat: hover시 작성자 이름 표시

--- client/src/components/home/Message.tsx
+++ client/src/components/home/Message.tsx
@@ -5,11 +5,12 @@ import { ReactComponent as Leaf3 } from "@src/assets/image/leaf3.svg";
 import { Link } from "react-router-dom";
 import { MessageProps } from "@src/types/home";
 import { styled } from "styled-components";
+import theme from "@src/styles/theme";
 
 const Message = (props: MessageProps) => {
   const {
     idx,
-    messageData: { _id },
+    messageData: { _id, name },
   } = props;
   const imgIdx = idx % 4;
   let absoluteTop = 0;
@@ -34,6 +35,7 @@ const Message = (props: MessageProps) => {
   return (
     <St.MessageContainer absolutetop={absoluteTop} absoluteleft={absoluteLeft}>
       <Link to={`message/${_id}`}>{{ 0: <Leaf0 />, 1: <Leaf1 />, 2: <Leaf2 />, 3: <Leaf3 /> }[imgIdx]}</Link>
+      <St.HoverText>From. {name}</St.HoverText>
     </St.MessageContainer>
   );
 };
@@ -45,5 +47,24 @@ const St = {
     position: absolute;
     top: ${(props) => props.absolutetop}rem;
     left: ${(props) => props.absoluteleft}rem;
+    &:hover > div {
+      opacity: 1;
+    }
+  `,
+  HoverText: styled.div`
+    position: absolute;
+    width: max-content;
+    padding: 0.5rem;
+    top: -10%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    color: ${theme.colors.primary};
+    background-color: white;
+    border: 2px solid ${theme.colors.primary};
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    pointer-events: none;
+    ${theme.fonts.Body1}
   `,
 };
